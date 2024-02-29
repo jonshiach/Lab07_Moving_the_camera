dd895b6ae5df2d9d12fec4c5016218647aad3664
Jon Shiach <jon.shiach@gmail.com>
2024-02-29 16:34:21 +0000

    First commit

--- source/main.cpp
+++ source/main.cpp
@@ -12,8 +12,6 @@
 #include "shader.hpp"
 #include "texture.hpp"
 #include "camera.hpp"
-#include "model.hpp"
-//#include "camera_exercises.hpp"
 
 // Create camera object
 Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
@@ -80,14 +78,140 @@ int main( void )
     // Compile shader program
     GLuint shaderID = LoadShaders("vertexShader.vert", "fragmentShader.frag");
     
+    // Use the shader program
+    glUseProgram(shaderID);
+    
+    // Create OpenGL textures
+    GLuint texture1 = loadBMP_custom("crate.bmp");
+    
     // Get the handles for the shader uniforms
+    GLuint texture1ID = glGetUniformLocation(shaderID, "texture1Sampler");
     GLuint modelID = glGetUniformLocation(shaderID, "model");
     GLuint viewID = glGetUniformLocation(shaderID, "view");
     GLuint projectionID = glGetUniformLocation(shaderID, "projection");
     
-    // Load suzanne object
-    model suzanne("teapot.obj");
-    suzanne.loadTexture("blue.bmp");
+    // Create the Vertex Array Object (VAO)
+    GLuint vertexArray;
+    glGenVertexArrays(1, &vertexArray);
+    glBindVertexArray(vertexArray);
+    
+    // Define vertex co-ordinates
+    static const GLfloat vertices[] = {
+        // front
+        -1.0f, -1.0f,  1.0f,    //              + ------ +
+         1.0f, -1.0f,  1.0f,    //             /|       /|
+         1.0f,  1.0f,  1.0f,    //   y        / |      / |
+        -1.0f, -1.0f,  1.0f,    //   |       + ------ +  |
+         1.0f,  1.0f,  1.0f,    //   + - x   |  + ----|- +
+        -1.0f,  1.0f,  1.0f,    //  /        | /      | /
+        // right                // z         |/       |/
+         1.0f, -1.0f,  1.0f,    //           + ------ +
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+        // back
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        // left
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+        // bottom
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+        // top
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+    };
+    
+    // Define texture vertices
+    static const GLfloat uvCoords[] = {
+        // front
+        0.0f, 0.0f,     // vertex co-ordinates are the same for each side
+        1.0f, 0.0f,     // of the cube so repeat every six vertices
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // right
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // back
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // left
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // bottom
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+        0.0f, 0.0f,
+        // top
+        1.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        0.0f, 1.0f,
+    };
+    
+    // Create Vertex Buffer Object
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // Create uv buffer
+    GLuint uvBuffer;
+    glGenBuffers(1, &uvBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(uvCoords), uvCoords, GL_STATIC_DRAW);
+    
+    // Cube positions
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -10.0f),
+        glm::vec3(-3.0f, -2.0f, -3.0f),
+        glm::vec3(-4.0f, -2.0f, -8.0f),
+        glm::vec3( 2.0f, -1.0f, -4.0f),
+        glm::vec3(-4.0f,  3.0f, -8.0f),
+        glm::vec3( 3.0f, -2.0f, -5.0f),
+        glm::vec3( 4.0f,  2.0f, -5.0f),
+        glm::vec3( 2.0f,  0.0f, -2.0f),
+        glm::vec3(-1.0f,  1.0f, -2.0f)
+    };
     
     do {
         // Update timers
@@ -98,24 +222,47 @@ int main( void )
         // Clear the window
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+        // Bind the textures
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(texture1ID, 0);
+        
+        // Send the vertex buffer to the shaders
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        // Send the uv buffer to the shaders
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
         // Get the view and projection matrices from the camera library
-        camera.calculateMatrices(window, deltaTime);
+        camera.calculateMatrices();
         glm::mat4 view = camera.getViewMatrix();
         glm::mat4 projection = camera.getProjectionMatrix();
         
-        // Calculate model matrix
-        glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f, 0.0f));
-        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f));
-        glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        glm::mat4 model = translate * rotate * scale;
+        // Loop through cubes and draw each one
+        for (int i = 0; i < 10; i++)
+        {
+            // Calculate model matrix
+            glm::mat4 translate = glm::translate(glm::mat4(1.0f), cubePositions[i]);
+            glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f));
+            glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), glm::radians(30.0f * i), glm::vec3(1.0f, 1.0f, 0.0f));
+            glm::mat4 model = translate * rotate * scale;
+            
+            // Send the model view and projection matrices to the shader
+            glUniformMatrix4fv(modelID, 1, GL_FALSE, &model[0][0]);
+            glUniformMatrix4fv(viewID, 1, GL_FALSE, &view[0][0]);
+            glUniformMatrix4fv(projectionID, 1, GL_FALSE, &projection[0][0]);
+            
+            // Draw the triangles
+            glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / (sizeof(float) * 3));
+        }
         
-        // Send the model, view and projection matrices to the shader
-        glUniformMatrix4fv(modelID, 1, GL_FALSE, &model[0][0]);
-        glUniformMatrix4fv(viewID, 1, GL_FALSE, &view[0][0]);
-        glUniformMatrix4fv(projectionID, 1, GL_FALSE, &projection[0][0]);
-        
-        // Draw object
-        suzanne.draw(shaderID);
+        // Disable vertex buffer objects
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
         
         // Swap buffers
         glfwSwapBuffers(window);
@@ -127,8 +274,10 @@ int main( void )
            glfwWindowShouldClose(window) == 0 );
 
     // Cleanup
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &uvBuffer);
+    glDeleteVertexArrays(1, &vertexArray);
     glDeleteProgram(shaderID);
-    suzanne.deleteBuffers();
     
     // Close OpenGL window and terminate GLFW
     glfwTerminate();

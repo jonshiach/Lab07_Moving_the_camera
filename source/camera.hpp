5db713b1fcbf67762e45ec5ea20d3c8ef7140940
Jon Shiach <60979354+jonshiach@users.noreply.github.com>
2024-02-29 16:26:21 +0000

    Update camera.hpp

--- source/camera.hpp
+++ source/camera.hpp
@@ -14,9 +14,6 @@ public:
     
     // Camera vectors
     glm::vec3 position;
-    glm::vec3 front;
-    glm::vec3 right;
-    glm::vec3 up;
     glm::vec3 worldUp;
     
     // Transformation matrices
